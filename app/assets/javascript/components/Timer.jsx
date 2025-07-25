34b00f184a935882be2a85d999133d2a92c2558d
Hamdi <32712674+noban64@users.noreply.github.com>
2025-07-25 11:15:22 +0100

    fixed timer layout

--- app/assets/javascript/components/Timer.jsx
+++ app/assets/javascript/components/Timer.jsx
@@ -1,25 +1,40 @@
-import React ,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 export default () => {
-const [timer, setTimer] = useState(0);
-function incrementTimer() {
-    newTimer = timer+1 
-    setTimer(newTimer)
-}
+  const [second, setSecond] = useState(0);
+  const [minute, setMinute] = useState(0);
+  const [hour, setHour] = useState(0);
+  function incrementTimer() {
+    newTimer = second + 1;
+    if (newTimer >= 60) {
+      newTimer = 0;
+      minuteTimer = minute + 1;
 
-useEffect(() => {
+      if (minuteTimer >= 60) {
+        minuteTimer = 0;
+        hourTimer = hour + 1;
+        setHour(hourTimer);
+      }
+      setMinute(minuteTimer);
+    }
+    setSecond(newTimer);
+  }
 
-    const key = setInterval(() => { 
-   incrementTimer()},1000 
-    )
-
+  useEffect(() => {
+    const key = setInterval(() => {
+      incrementTimer();
+    }, 1000);
 
     return () => {
-        clearInterval(key);
-    }
-})
-
-return(
- <div>{timer}</div>
-) 
-}
+      clearInterval(key);
+    };
+  });
+  function format(n) {
+    return n > 9 ? "" + n : "0" + n;
+  }
+  return (
+    <div id="timer">
+      {format(hour)}:{format(minute)}:{format(second)}
+    </div>
+  );
+};
