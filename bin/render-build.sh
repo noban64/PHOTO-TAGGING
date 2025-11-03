89425c75ced2b5d323678b31fd6e74a94c4ca42c
Hamdi <32712674+noban64@users.noreply.github.com>
2025-11-03 21:14:28 +0000

    Changed build script to build node items instead of through asset pipleline

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,9 +5,7 @@ set -o errexit
 
 bundle install
 npm install
-npm run build 
-bin/rails assets:precompile
-bin/rails assets:clean
+npm run build
+bin/rails db:seed
 bin/rails db:migrate
 
-bin/rails routes 
